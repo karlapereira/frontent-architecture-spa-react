704d05c10dcabc8c38ea779cb4ca776b8febb35f
Karla Pereira <karlapereira@gec.inatel.br>
2022-10-27 20:32:39 -0300

    structure SPA base

---
+++ movies/src/api/MoviesService.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const API_KEY = 'e46d210fe9992edff7ba1ce02c3eb5eb';
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const withBaseUrl = path => `${BASE_URL}${path}?api_key=${API_KEY}`
+
+export class MoviesService {
+    static getMovies() {
+        return axios(withBaseUrl('movie/popular'));
+    }
+}
---
+++ movies/src/styles/MainMenuContainer.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const MainMenuContainer = styled.nav`
+    ul {
+        list-styled: none;
+        padding: 0;
+
+        li {
+            display: inline-block;
+            margin-right: 10px;
+        }
+    }
+`
